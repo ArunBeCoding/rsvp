85e37635c43e26be8451d27d6662575b7d374c33
Ravi Arun Kumarr <53741078+ArunBeCoding@users.noreply.github.com>
2025-06-01 12:52:26 -0700

    modify eslint

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // Only run ESLint on these directories during `next build`
+    dirs: ['pages', 'utils', 'components', 'lib'],
+    // Or disable ESLint during production builds
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
